0f6540b9f11cf4696db1f651c605f7da3ec7ee14
KushalKantiMehta <kushalkanti9697@gmail.com>
2023-02-26 18:06:01 +0530

    Page not Found added

--- frontend/src/screens/NotFound.jsx
+++ frontend/src/screens/NotFound.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import PAGENOTFOUND from '../assets/PAGENOTFOUND.svg'
+import './NotFound.styles.css'
 const NotFound = () => {
-  return <div>NotFound</div>
+  return (
+    <div className='notFound'>
+      <img src={PAGENOTFOUND} alt='Page Not Found' />
+    </div>
+  )
 }
 
 export default NotFound
