efa40fbbc60154e73ac2b2e1a160bc3535c1b641
KushalKantiMehta <kushalkanti9697@gmail.com>
2023-03-27 09:59:17 +0530

    added axios , highcharts, react-highcharts-official

---
+++ frontend/src/axios/axios.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+// get world covid data
+const worldRequest = {
+  method: 'GET',
+  url: 'https://corona-virus-world-and-india-data.p.rapidapi.com/api',
+  headers: {
+    'X-RapidAPI-Key': 'c8208a25cbmshad1ebaef0e01d95p1f67c5jsn18e7ec37ebd2',
+    'X-RapidAPI-Host': 'corona-virus-world-and-india-data.p.rapidapi.com',
+  },
+}
+
+export const getWorldData = axios
+  .request(worldRequest)
+  .then(function (response) {
+    return response
+  })
+  .catch(function (error) {
+    console.error(error)
+    return error
+  })
